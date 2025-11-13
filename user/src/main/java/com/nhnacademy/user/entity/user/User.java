3d6a474a203d35ef6fef638a1de49073aada9fd4
jin000oo <wlsdud3309@naver.com>
2025-11-13 13:29:01 +0900

    feat: 엔티티 구현 및 패스워드 인코더 설정 추가

---
+++ user/src/main/java/com/nhnacademy/user/entity/user/User.java
@@ -0,0 +1,93 @@
+/*
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * + Copyright 2025. NHN Academy Corp. All rights reserved.
+ * + * While every precaution has been taken in the preparation of this resource,  assumes no
+ * + responsibility for errors or omissions, or for damages resulting from the use of the information
+ * + contained herein
+ * + No part of this resource may be reproduced, stored in a retrieval system, or transmitted, in any
+ * + form or by any means, electronic, mechanical, photocopying, recording, or otherwise, without the
+ * + prior written permission.
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ */
+
+package com.nhnacademy.user.entity.user;
+
+import com.nhnacademy.user.entity.account.Account;
+import com.nhnacademy.user.entity.address.Address;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+@Table(name = "Users")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {     // 회원 기본 정보
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_created_id")
+    private Long userCreatedId;             // 회원 고유 ID (PK, AI)
+
+    @Length(max = 30)
+    @Column(name = "user_name", nullable = false)
+    private String userName;                // 회원 이름
+
+    @Length(max = 30)
+    @Column(name = "phone_number", nullable = false, unique = true)
+    private String phoneNumber;             // 연락처 (UK)
+
+    @Length(max = 80)
+    @Column(nullable = false, unique = true)
+    private String email;                   // 이메일 (UK)
+
+    private LocalDate birth;                // 생년월일, YYYY-MM-DD 패턴
+
+    @Column(nullable = false)
+    private Grade grade = Grade.GENERAL;    // 회원 등급, default = 'GENERAL'
+
+    @Column(nullable = false)
+    private Status status = Status.ACTIVE;  // 계정 상태, default = 'ACTIVE'
+
+    @Column(nullable = false)
+    private long point = 0L;                 // 보유 포인트, default = 0
+
+    @CreationTimestamp
+    @Column(name = "joined_at", nullable = false)
+    private LocalDateTime joinedAt;         // 가입일시
+
+    @Column(name = "last_login_at")
+    private LocalDateTime lastLoginAt;      // 최근 로그인 일시
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Account account;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Address> addresses = new ArrayList<>();
+
+    public User(String userName, String phoneNumber, String email, LocalDate birth) {
+        this.userName = userName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.birth = birth;
+        this.grade = Grade.GENERAL;
+        this.status = Status.ACTIVE;
+        this.point = 0L;
+    }
+
+}
